29389e66ee6591a19d60e635876c4044c787e2ad
Igor <igorshelopaev@gmail.com>
2024-07-04 14:24:17 +0200

    Update ContentView.swift

--- openai-async-image-swiftui-example/ContentView.swift
+++ openai-async-image-swiftui-example/ContentView.swift
@@ -11,32 +11,34 @@ import SwiftUI
 struct ContentView: View {
     
     @State var text: String = "Sun"
-
     @State var imageText: String = "Sun"
 
-    var customImageTpl : some View{
-        OpenAIAsyncImage(prompt: $imageText, size: .dpi1024){ state in
-            switch state{
-                case .loaded(let image) :
-                image
-                    .resizable()
-                    .scaledToFill()
-            case .loadError(let error) : Text(error.localizedDescription)
-                case .loading : ProgressView()
+    var customImageTpl: some View {
+        OpenAIAsyncImage(prompt: $imageText, size: .dpi1024) { state in
+            switch state {
+                case .loaded(let image):
+                    image
+                        .resizable()
+                        .scaledToFill()
+                case .loadError(let error):
+                    Text(error.localizedDescription)
+                        .padding()
+                case .loading:
+                    ProgressView()
             }
         }
     }
     
-    var defaultImageTpl: some View{
+    var defaultImageTpl: some View {
         OpenAIAsyncImage(prompt: .constant("Sun"))
-                       .frame(width: 125, height: 125)
-                       .padding(.top, 75)
-                       .padding(.trailing, 20)
+            .frame(width: 125, height: 125)
+            .padding(.top, 75)
+            .padding(.trailing, 20)
     }
     
     var body: some View {
         VStack {
-            HStack{
+            HStack {
                 Spacer()
                 defaultImageTpl
             }
@@ -53,20 +55,15 @@ struct ContentView: View {
         }
         .background(customImageTpl)
         .ignoresSafeArea()
-        
     }
 }
-
 
 fileprivate struct RoundedTextFieldStyle: TextFieldStyle {
     func _body(configuration: TextField<Self._Label>) -> some View {
         configuration
             .padding(.vertical)
             .padding(.horizontal, 24)
-            .background(
-                .ultraThinMaterial
-            )
+            .background(.ultraThinMaterial)
             .clipShape(Capsule(style: .continuous))
     }
 }
-

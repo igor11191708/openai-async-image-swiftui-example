50c43e4ce03bf6baf763643034a0420aa062d3e0
Igor <igorshelopaev@gmail.com>
2024-07-07 16:55:31 +0200

    Update TextWithLinks.swift

--- openai-async-image-swiftui-example/view/TextWithLinks.swift
+++ openai-async-image-swiftui-example/view/TextWithLinks.swift
@@ -14,10 +14,9 @@ struct TextWithLinks: View {
     var lineSpacing: CGFloat = 8 // Customize the line spacing as needed
     
     var body: some View {
-        // Detect and split the input string into parts (text and links)
         let parts = detectAndSplitLinks(from: inputString)
-        // Parse the parts and apply line spacing
-        return parseString(parts: parts)
+
+        parseString(parts: parts)
             .lineSpacing(lineSpacing)
     }
     
@@ -29,13 +28,12 @@ struct TextWithLinks: View {
         VStack(alignment: .leading, spacing: lineSpacing) {
             ForEach(parts.indices, id: \.self) { index in
                 let part = parts[index]
-                // Check if the part is a link
                 if part.starts(with: "http://") || part.starts(with: "https://") {
                     if let url = URL(string: part) {
-                        Link(part, destination: url) // Wrap the link in a Link view
+                        Link(part, destination: url)
                     }
                 } else {
-                    Text(part) // Wrap the text in a Text view
+                    Text(part)
                 }
             }
         }
@@ -44,10 +42,13 @@ struct TextWithLinks: View {
 
 /// Detects HTTP links in a string and splits the string into an array of text and links.
 /// - Parameter input: The input string containing text and HTTP links.
-/// - Returns: An array of strings with text and links separated.
+/// - Returns: An array of strings with text and links separated. If the regex fails, returns the original input string.
 fileprivate func detectAndSplitLinks(from input: String) -> [String] {
     // Define the regular expression pattern for HTTP/HTTPS links
-    let regex = try! NSRegularExpression(pattern: "https?://[a-zA-Z0-9./?=_-]+")
+    guard let regex = try? NSRegularExpression(pattern: "https?://[a-zA-Z0-9./?=_-]+") else {
+        return [input]
+    }
+    
     let nsString = input as NSString
     let results = regex.matches(in: input, range: NSRange(location: 0, length: nsString.length))
     
